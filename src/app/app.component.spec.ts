a882c08ca1442c5add61f50d7dfb80c0b35e64e8
Simone Antunes <sanntosti@gmail.com>
2021-04-28 22:10:56 -0300

    Atualização

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,13 @@
 import { TestBed } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
+import { ConversorModule } from './conversor';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
+      imports: [ConversorModule],
     }).compileComponents();
   });
 
